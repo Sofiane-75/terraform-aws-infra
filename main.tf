df9b9090e5704fc3e1db452d0a270ebff1a2e199
Sofiane-75 <solrn80@gmail.com>
2025-12-31 12:46:02 +0100

    chore : configure terraform backend

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 # Main Terraform configuration
 # Resources will be added incrementally (EC2, S3)
 
+
+# backend "s3" {
+#   bucket = "my-terraform-state-bucket"
+#   key    = "dev/terraform.tfstate"
+#   region = "eu-west-3"
+# }
+
+
+
+terraform {
+  required_version = ">= 1.5"
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
