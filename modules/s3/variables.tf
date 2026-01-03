da37bd24adde207a463d62263199a56889a64184
Sofiane-75 <solrn80@gmail.com>
2026-01-03 21:18:12 +0100

    feat: define s3 modules variables

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,16 +1,10 @@
 variable "bucket_name" {
-  description = "Nom du bucket S3"
+  description = "Name of the S3 bucket"
   type        = string
 }
 
 variable "environment" {
-  description = "Environnement (dev, prod)"
+  description = "Deployment environment (dev, prod)"
   type        = string
-}
-
-variable "bucket_name" {
-  description = "S3 bucket name"
-  type        = string
-  default     = "sofiane-terraform-demo-bucket"
 }
 
