c28d74a1fe3b87687f0b756d64b1fc79dd80f4d9
Sofiane-75 <solrn80@gmail.com>
2026-01-03 21:20:53 +0100

    feat: expose S3 outputs from root module

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "ec2_instance_id" {
-  description = "EC2 instance ID from the EC2 module"
-  value       = module.ec2.instance_id
+output "s3_bucket_name" {
+  description = "S3 bucket name (from module)"
+  value       = module.s3.s3_bucket_name
 }
 
-output "s3_bucket_name" {
-  description = "S3 bucket name from the S3 module"
-  value       = module.s3.s3_bucket_name
+output "s3_bucket_arn" {
+  description = "S3 bucket ARN (from module)"
+  value       = module.s3.s3_bucket_arn
 }
 
