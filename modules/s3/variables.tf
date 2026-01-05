ffc7623e49fb3e44290419f7b10e88659a654494
Sofiane-75 <solrn80@gmail.com>
2026-01-05 18:29:25 +0100

    feat: add validation rules to S3 module variables

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -8,3 +8,13 @@ variable "environment" {
   type        = string
 }
 
+validation {
+  condition     = length(var.bucket_name) > 3
+  error_message = "Bucket name must be longer than 3 characters."
+}
+
+validation {
+  condition     = contains(["dev", "prod"], var.environment)
+  error_message = "Environment must be dev or prod."
+}
+
