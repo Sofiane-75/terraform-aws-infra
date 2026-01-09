a5bb7a3b14034146d3669f8ee72b9869aa4edf64
Sofiane-75 <solrn80@gmail.com>
2026-01-09 10:41:50 +0100

    feat: protect S3 bucket with prevent_destroy lifecycle rule

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,10 +1,13 @@
-
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 
   tags = {
     Name        = var.bucket_name
     Environment = var.environment
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
