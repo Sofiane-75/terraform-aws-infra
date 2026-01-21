2f5e4a46a1c18f25401ffbe1167050fcc2f10bce
Sofiane-75 <solrn80@gmail.com>
2026-01-21 23:58:53 +0100

    docs(outputs): improve output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "s3_bucket_name" {
-  description = "S3 bucket name (from module)"
+  description = "Name of the S3 bucket created by the S3 module"
   value       = module.s3.s3_bucket_name
 }
 
 output "s3_bucket_arn" {
-  description = "S3 bucket ARN (from module)"
+  description = "ARN of the S3 bucket created by the S3 module"
   value       = module.s3.s3_bucket_arn
 }
 
 output "ec2_instance_id" {
-  description = "EC2 instance ID"
+  description = "ID of the EC2 instance created by the EC2 module"
   value       = module.ec2.instance_id
 }
 
