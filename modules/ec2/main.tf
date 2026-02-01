75d2169b360dbf618cdff816c56cdca86f1273a6
Sofiane-75 <solrn80@gmail.com>
2026-02-01 23:44:06 +0100

    docs(ec2): add resource header comment

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,7 @@
+# EC2 module main resource definition
+# This resource provisions a single EC2 instance
+
+
 resource "aws_instance" "this" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (exemple)
   instance_type = var.instance_type
