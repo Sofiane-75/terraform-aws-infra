4d7e1f835371cb50bedf310ee468b3c40796c073
Sofiane-75 <solrn80@gmail.com>
2026-03-13 13:06:34 +0100

    doc : var

--- variables.tf
+++ variables.tf
@@ -6,18 +6,20 @@ variable "project_name" {
   default     = "terraform-aws-infra"
 }
 
-
+#Region
 variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "eu-west-1"
 }
 
+#Bucket name
 variable "bucket_name" {
   description = "S3 bucket name"
   type        = string
 }
 
+#Environement
 variable "environment" {
   description = "Environment name (dev, prod)"
   type        = string
